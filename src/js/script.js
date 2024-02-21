b186f82e2fd508dac56e6786b6039f9f8bbc63e8
Oleksandr <sidrsidr2@gmail.com>
2024-02-21 12:14:09 +0200

    header styling & modal added

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,26 @@
+const btn = document.querySelectorAll('.main_btn');
+const modal = document.querySelector('.modal');
+const close = document.querySelector('.modal-close');
+
+btn.forEach(item=>{
+    item.addEventListener('click',()=>{
+        modal.style.display = 'block';
+        document.body.style.overflow = 'hidden';
+    })
+})
+
+close.addEventListener('click',()=>{
+    modal.style.display = 'none';
+    document.body.style.overflow = '';
+})
+
+modal.addEventListener('click',(e)=>{
+    if(e.target === modal){
+        modal.style.display = 'none';
+        document.body.style.overflow = '';
+    }
+})
+
 window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.menu'),
     menuItem = document.querySelectorAll('.menu_item'),
